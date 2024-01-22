d0710863dd67ba3350ee4ec0c99db65fea485e16
Marko Jekrić <markojerkic266@gmail.com>
2024-01-22 19:57:52 +0100

    save to local storage

--- src/stores/likes.ts
+++ src/stores/likes.ts
@@ -1,22 +1,40 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
 
-export const useLikedPokemonStore = defineStore('counter', () => {
-  const likesSet = ref(new Set())
-  const likes = computed(() => Array.from(likesSet.value))
-  const likesCount = computed(() => likesSet.value.size)
+export const useLikedPokemonStore = defineStore("counter", () => {
+  let initialValues = [];
+  const localStorageItems = localStorage.getItem("likes");
+  try {
+    if (localStorageItems) {
+      try {
+        const obj = JSON.parse(localStorageItems);
+        if (obj instanceof Array) {
+          initialValues = obj;
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  } catch (e) {
+    console.error(e);
+  }
+  const likesSet = ref(new Set(initialValues));
+  const likes = computed(() => Array.from(likesSet.value));
+  const likesCount = computed(() => likesSet.value.size);
 
   function addLike(name: string) {
-    likesSet.value.add(name)
+    likesSet.value.add(name);
+    localStorage.setItem("likes", JSON.stringify(Array.from(likesSet.value)));
   }
 
   function isLiked(name: string) {
-    return likesSet.value.has(name)
+    return likesSet.value.has(name);
   }
 
   function removeLike(name: string) {
-    likesSet.value.delete(name)
+    likesSet.value.delete(name);
+    localStorage.setItem("likes", JSON.stringify(Array.from(likesSet.value)));
   }
 
-  return { likes, addLike, likesCount, isLiked, removeLike }
-})
+  return { likes, addLike, likesCount, isLiked, removeLike };
+});
